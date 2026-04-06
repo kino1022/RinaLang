52adca885230ca13896edd47e63df7b48338dc74
kino1022 <132138562+kino1022@users.noreply.github.com>
2026-04-06 19:21:09 +0900

    udpate

---
+++ public/runtime/entity.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <typeindex>
+#include <unordered_map>
+
+#include "exceptions.hpp"
+
+namespace rinalang::entity {
+    
+    struct i_component_holder {
+        
+        virtual ~i_component_holder() = default;
+        
+        virtual void* ptr() =0;
+        
+        virtual const void* ptr() const =0;
+        
+    };
+    
+    template<class T>
+    struct component_holder final : i_component_holder {
+        template <class... Args>
+        explicit component_holder(Args&&... args) : value(std::forward<Args>(args)...) {}
+
+        void* ptr() override { return &value; }
+        const void* ptr() const override { return &value; }
+
+        T value;
+    };
+    
+    class entity_base {
+        
+    public:
+        entity_base() = default;
+        virtual ~entity_base() = default;
+        
+        entity_base(const entity_base&) = delete;
+        entity_base& operator=(const entity_base&) = delete;
+        entity_base(entity_base&&) = default;
+        entity_base& operator=(entity_base&&) = default;
+        
+        template<class T, class... Args>
+        T& add (Args&&... args) {
+            const std::type_index key(typeid(T));
+            if (components.contains(key)) {
+                components.erase(key);
+            }
+            auto holder = std::make_unique<component_holder<T>>(std::forward<Args>(args)...);
+            T& ref = holder->value;
+            components.emplace(key, std::move(holder));
+            return ref;
+        }
+        
+    private:
+        std::unordered_map<std::type_index, std::unique_ptr<i_component_holder>> components;
+    };
+}
---
+++ public/runtime/exceptions.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdexcept>
+
+namespace rinalang::exceptions {
+    
+    struct runtime_exception : std::runtime_error {
+        using std::runtime_error::runtime_error;
+    };
+    
+    struct component_notfound_exception : runtime_exception {
+        using runtime_exception::runtime_exception;
+    };
+    
+    struct component_same_type_exception : runtime_exception {
+        using runtime_exception::runtime_exception;
+    };
+    
+}
